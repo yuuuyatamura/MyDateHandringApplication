5c1e10bf7eb2cda89bbcd14b34ee10be7a19a0c5
yuytamura <yt0829xwp@gmail.com>
2020-05-05 01:48:31 +0900

    create new file

--- src/main/java/com/example/Date/Handring/controller/DateHandringController.java
+++ src/main/java/com/example/Date/Handring/controller/DateHandringController.java
@@ -1,5 +1,41 @@
 package com.example.Date.Handring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class DateHandringController {
 
+	@GetMapping("/date")
+	public String getDate() {
+
+		return "DateRegister";
+	}
+
+	@PostMapping("/date")
+	public String postRequest() {
+
+		return "DateRegister";
+	}
+
+	@GetMapping("/date/handring")
+	public String getHandring() {
+		return "DateHandring";
+	}
+	@PostMapping("/date/handring")
+	public String postHandringRequest() {
+
+		return "DateHandring";
+	}
+
+	@GetMapping("/date/update")
+	public String getUpdate() {
+		return "DateUpdate";
+	}
+	@PostMapping("/date/update")
+	public String postUpdate() {
+
+		return "DateUpdate";
+	}
 }
