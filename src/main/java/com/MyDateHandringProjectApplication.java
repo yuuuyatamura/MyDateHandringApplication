a44ec72a60c035efdd9dbb4bcd73956f6a4a7f0b
yuytamura <yt0829xwp@gmail.com>
2020-05-04 15:17:52 +0900

    Project基板

---
+++ src/main/java/com/MyDateHandringProjectApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDateHandringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDateHandringProjectApplication.class, args);
+	}
+
+}
