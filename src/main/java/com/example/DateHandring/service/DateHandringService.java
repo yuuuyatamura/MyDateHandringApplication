e51b5ae79c4cf2c7319af1d34bfbef2926c3901f
yuuuyatamura <49576718+yuuuyatamura@users.noreply.github.com>
2020-05-24 09:33:03 +0900

    Createまで実装完了

--- src/main/java/com/example/DateHandring/service/DateHandringService.java
+++ src/main/java/com/example/DateHandring/service/DateHandringService.java
@@ -1,26 +1,36 @@
 package com.example.DateHandring.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.DateHandring.domain.DateFormula;
+import com.example.DateHandring.repository.DateHandringRepository;
 
 @Service
-public interface DateHandringService {
+public class DateHandringService {
 
+	@Autowired
+	private DateHandringRepository repository;
 
+	public void register(DateFormula dateFormula) {
+		repository.insert(dateFormula);
 
-	//ここで、新規の日付を登録する
-	void insert(DateFormula dateFormula);
+	}
 
-//	//1件検索
-//	public void select(DateFormula dateFormula) {
-//		// TODO 自動生成されたメソッド・スタブ
-//	}
+	public List<DateFormula> search(String id) {
+		return repository.select();
+	}
 
-//	//全件検索
-//	public void selectAll(DateFormula dateFormula) {
-//		// TODO 自動生成されたメソッド・スタブ
-//	}
+	public String calculate(String baseDate, DateFormula formula) {
+		LocalDate date = LocalDate.parse(baseDate, DateTimeFormatter.ofPattern("yyyyMMdd"));
+//		System.out.println("date:"+ date);
+		LocalDate calculatedDate = date.plusYears(formula.getYear()).plusMonths(formula.getMonth()).plusDays(formula.getDay());
+//		System.out.println("calculatedDate.format(DateTimeFormatter.ofPattern(\"yyyyMMdd\"):" + calculatedDate.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+		return calculatedDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+	}
 
-	//更新
 }
--- src/main/java/com/example/DateHandring/service/DateHandringServiceImpl.java
+++ src/main/java/com/example/DateHandring/service/DateHandringServiceImpl.java
@@ -1,21 +1,23 @@
-package com.example.DateHandring.service;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.DateHandring.domain.DateFormula;
-
-@Service
-public class DateHandringServiceImpl implements DateHandringService {
-
-	private DateHandringDAO dao;
-
-	@Autowired
-	public void DateHandringDAOImpl(DateHandringDAO dao) {
-		this.dao = dao;
-	}
-	public void insert(DateFormula dateFormula) {
-		dao.insert(dateFormula);
-
-	}
-
-}
+//package com.example.DateHandring.service;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//
+//import com.example.DateHandring.repository.DateHandringRepository;
+//
+//@Service
+//@Component
+//public class DateHandringServiceImpl{
+//
+//	private DateHandringDAO dao;
+//
+//	@Autowired
+//	public void DateHandringDAOImpl(DateHandringDAO dao) {
+//		this.dao = dao;
+//	}
+//	public void insert(DateHandringRepository dateHandringRepository) {
+//		dao.insert(dateHandringRepository);
+//
+//	}
+//
+//}
