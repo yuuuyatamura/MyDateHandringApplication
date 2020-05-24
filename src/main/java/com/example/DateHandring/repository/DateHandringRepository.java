98771f3f05097412f67b8e823345f23144abbfdb
yuuuyatamura <49576718+yuuuyatamura@users.noreply.github.com>
2020-05-24 09:32:10 +0900

    Createまで実装完了

--- src/main/java/com/example/DateHandring/repository/DateHandringRepository.java
+++ src/main/java/com/example/DateHandring/repository/DateHandringRepository.java
@@ -1,5 +1,26 @@
 package com.example.DateHandring.repository;
 
-public class DateHandringRepository {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.DateHandring.domain.DateFormula;
+
+
+//@MapperScan(basePackages = {"com.example.DateHandring.service.DateHandringService"})
+@Mapper
+public interface DateHandringRepository {
+
+	/**
+	 * 日付計算式を更新します。
+	 *
+	 * @param formula 登録する日付計算式
+	 */
+	@Insert("INSERT INTO dateformula VALUES(#{id}, #{name}, #{year}, #{month}, #{day})")
+	void insert(DateFormula formula);
+
+	@Select("SELECT * FROM dateformula ORDER BY id ASC")
+	List<DateFormula> select();
 }
