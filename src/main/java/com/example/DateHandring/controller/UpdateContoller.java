ff2c8ccc5e6314addae14aeffe3935c70fa28e44
yuuuyatamura <49576718+yuuuyatamura@users.noreply.github.com>
2020-06-20 12:30:53 +0900

    UpdateControllerエラーハンドリング修正

--- src/main/java/com/example/DateHandring/controller/UpdateContoller.java
+++ src/main/java/com/example/DateHandring/controller/UpdateContoller.java
@@ -3,28 +3,42 @@ package com.example.DateHandring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.DateHandring.domain.DateFormula;
+import com.example.DateHandring.domain.SimulationForm;
 import com.example.DateHandring.service.DateHandringService;
 
 @Controller
-@RequestMapping("/update/{dateId}")
+@RequestMapping("/update/{id}")
 public class UpdateContoller {
 
 	@Autowired
 	private DateHandringService service;
 
-	@GetMapping("/update")
+//	@GetMapping("/update/{id}")
+	@GetMapping
 	public String index(@PathVariable String id, Model model) {
-		model.addAttribute("dataFormula", service.search(id));
+		model.addAttribute("dateFormula", service.search(id));
 		return "DateUpdate";
 	}
-	@PostMapping("/update")
-	public String postUpdate() {
 
-		return "DateUpdate";
+//	@PostMapping("/update/{id}")
+	@PostMapping
+	public String update(@ModelAttribute @Validated DateFormula dataFormula, BindingResult bindingresult, Model model) {
+		if(bindingresult.hasErrors()) {
+			return "DateUpdate";
+		}
+
+		System.out.println("★update");
+		service.update(dataFormula);
+		model.addAttribute("simulationForm", new SimulationForm());
+		return "DateHandring";
 	}
 }
