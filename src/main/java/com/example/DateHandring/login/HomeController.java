50be1e84800e0fb474691b1a2849bafd9ceaabea
yuytamura <yt0829xwp@gmail.com>
2020-06-24 22:51:27 +0900

    spring Security導入

---
+++ src/main/java/com/example/DateHandring/login/HomeController.java
@@ -0,0 +1,12 @@
+//package com.example.DateHandring.login;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//
+////ログインとかに使うコントローラ
+//@Controller
+//public class HomeController {
+//
+//	@Autowired
+//	UserService userService;
+//}
